42c7284782cc547a1f6002cfb25aec71ffb51c80
Brandon Fallin <brandonfallin@gmail.com>
2025-09-08 11:49:15 -0400

    fix(px4_telemetry): correct multiple takeoff bug
    Previously, in px4_telemetry, you could send multiple takeoff requests even if the drone was in flight. This fixes the issue by adding an if statement that checks landed_state_ in the joy_callback.

--- px4_telemetry/src/PX4Telemetry.cpp
+++ px4_telemetry/src/PX4Telemetry.cpp
@@ -150,7 +150,13 @@ void PX4Telemetry::joy_callback(const sensor_msgs::msg::Joy::SharedPtr joy_msg) 
                 //Arm
                 send_arming_request(true);
             } else {
-                send_tol_request(true);
+                //Check if vehicle in air
+                if (landed_state_ == on_ground) {
+                    //Send takeoff request
+                    send_tol_request(true);
+                } else {
+                    RCLCPP_ERROR(this->get_logger(), "Takeoff request ignored: vehicle not in landed state.");
+                }
             }
         }
 
